bb4e82ab0487a25a57f2592eb6511730d5793cdf
alschofield93 <alschofield93@gmail.com>
2021-02-13 00:00:02 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import { Provider } from 'react-redux'
 import Footer from './features/main-components/footer/Footer';
 import Nav from './features/main-components/nav/Nav';
 import Container from './features/components/Container/Container';
 import Home from './features/pages/home/Home';
 import './App.css';
 import { ThemeProvider, themes } from './themes';
-import store from './redux/store'
 
-class App extends React.Component {
-  state = {
+const App = ({}) => {
+  let [state, updateState] = useState({
     themeName: 'dark',
     theme: themes.dark
+  })
+
+  const handleThemeToggle = () => {
+    const name = (state.themeName === 'light') ? 'dark' : 'light';
+    updateState({ theme: themes[name], themeName: name });
   };
 
-  handleThemeToggle = () => {
-    const name = (this.state.themeName === 'light') ? 'dark' : 'light';
-    this.setState({ theme: themes[name], themeName: name });
-  };
+  return (
+    <Router>
+      <ThemeProvider theme={state.theme}>
+        <Container data-testid="app-container">
+          <Nav handleThemeToggle={handleThemeToggle} />
 
-  render() {
-    return (
-      <Router>
-        <Provider store={store}>
-          <ThemeProvider theme={this.state.theme}>
-            <Container data-testid="app-container">
-              <Nav handleThemeToggle={this.handleThemeToggle} />
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
 
-              <Switch>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-
-              <Footer />
-            </Container>
-          </ThemeProvider>
-        </Provider>
-      </Router>
-    );
-  }
+          <Footer />
+        </Container>
+      </ThemeProvider>
+    </Router>
+  );
 }
 
 export default App;
