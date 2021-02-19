182b6294b034f35994c30d39ec445b414a7d49e6
alschofield93 <alschofield93@gmail.com>
2021-02-19 18:31:25 -0500

    nav changes

--- README.md
+++ README.md
@@ -2,10 +2,3 @@
 - create a basic calculator to get better with react
 
 url: https://git.heroku.com/enigmatic-fjord-59171.git
-
-# specs
-- ='s operator
-- almost but not quite, the operations are happening in the wrong way, should be
-  - click operator
-  - then type number to apply operator with on the original number
-  - then apply the previous operations once the next operator is selected
--- src/features/main-components/nav/Nav.js
+++ src/features/main-components/nav/Nav.js
@@ -6,8 +6,9 @@ import { withTheme } from '../../../themes';
 const Nav  = ({theme, handleThemeToggle }) => {
   return (
     <Container theme={theme.nav.container} data-testid='nav-container'>
-      <NavItem theme={theme.nav.item} type='image' src={`${process.env.PUBLIC_URL}/scho-logo.png`} href='/' />
-      {/* <NavItem type="anchor" value="Merch" href="https://scho.clothing" /> */}
+      <NavItem theme={theme.nav.item} type='image' src={`${process.env.PUBLIC_URL}/scho-logo.png`} href='https://lit-scrubland-37427.herokuapp.com/' />
+      <NavItem theme={theme.nav.item} type="anchor" value="Calculator" href="/" />
+      <NavItem theme={theme.nav.item} type="anchor" value="Todos App" href="https://still-brushlands-04490.herokuapp.com/" />
       <NavItem theme={theme.nav.item} type='icon' icon='paint' onClick={handleThemeToggle} />
     </Container>
   );
