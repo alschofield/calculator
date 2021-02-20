1c9718a914dc1f8b90f129498e5e24eef7e9da3d
alschofield93 <alschofield93@gmail.com>
2021-02-19 22:24:00 -0500

    nav updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # goal
 - create a basic calculator to get better with react
 
-url: https://calculator-3fxewc5l0.vercel.app/
+url: https://calculator.alschofield.vercel.app/
--- src/features/main-components/nav/Nav.js
+++ src/features/main-components/nav/Nav.js
@@ -6,9 +6,11 @@ import { withTheme } from '../../../themes';
 const Nav = ({theme, handleThemeToggle }) => {
   return (
     <Container theme={theme.nav.container} data-testid='nav-container'>
-      <NavItem theme={theme.nav.item} type='image' src={`${process.env.PUBLIC_URL}/scho-logo.png`} href='https://portfolio-nvjl35cjm.vercel.app/' />
-      <NavItem theme={theme.nav.item} type="anchor" value="Calculator" href="/" />
-      <NavItem theme={theme.nav.item} type="anchor" value="Todos App" href="https://todolist-app-eight.vercel.app/" />
+      <NavItem theme={{ ...theme.nav.item, ...theme.nav.icon }} type='image' src={`${process.env.PUBLIC_URL}/scho-logo.png`} href='https://portfolio.alschofield.vercel.app/' />
+      <Container theme={theme.nav.linksContainer}>
+        <NavItem theme={{ ...theme.nav.item, ...theme.nav.link }} type="anchor" value="Calculator" href="/" />
+        <NavItem theme={{ ...theme.nav.item, ...theme.nav.link }} type="anchor" value="Todos App" href="https://todolist.alschofield.vercel.app/" />
+      </Container>
       <NavItem theme={theme.nav.item} type='icon' icon='paint' onClick={handleThemeToggle} />
     </Container>
   );
