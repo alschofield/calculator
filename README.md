5048555fae14bcedb9ce315256d842322183d14b
alschofield93 <alschofield93@gmail.com>
2021-02-19 21:16:59 -0500

    refresh url and update nav

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # goal
 - create a basic calculator to get better with react
 
-url: https://git.heroku.com/enigmatic-fjord-59171.git
+url: https://calculator-3fxewc5l0.vercel.app/
--- src/features/main-components/nav/Nav.js
+++ src/features/main-components/nav/Nav.js
@@ -6,9 +6,9 @@ import { withTheme } from '../../../themes';
 const Nav = ({theme, handleThemeToggle }) => {
   return (
     <Container theme={theme.nav.container} data-testid='nav-container'>
-      <NavItem theme={theme.nav.item} type='image' src={`${process.env.PUBLIC_URL}/scho-logo.png`} href='https://lit-scrubland-37427.herokuapp.com/' />
+      <NavItem theme={theme.nav.item} type='image' src={`${process.env.PUBLIC_URL}/scho-logo.png`} href='https://portfolio-nvjl35cjm.vercel.app/' />
       <NavItem theme={theme.nav.item} type="anchor" value="Calculator" href="/" />
-      <NavItem theme={theme.nav.item} type="anchor" value="Todos App" href="https://still-brushlands-04490.herokuapp.com/" />
+      <NavItem theme={theme.nav.item} type="anchor" value="Todos App" href="https://todolist-app-eight.vercel.app/" />
       <NavItem theme={theme.nav.item} type='icon' icon='paint' onClick={handleThemeToggle} />
     </Container>
   );
